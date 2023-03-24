c4df0da61f89f2c27e61fe84b1b978c2cde48729
Manuel-Smith <riddicksmith37@gmail.com>
2023-03-24 23:44:29 +0300

    address and person classes

---
+++ address.py
@@ -0,0 +1,6 @@
+class Address:
+    def __init__(self, country, state, city, street, postal):
+        self.country = country
+        self.state = state
+        self.city = city
+        self.postal_code = postal
---
+++ person.py
@@ -0,0 +1,30 @@
+from address import Address
+from aifc import Error
+
+
+class Person:
+    def __init__(self, first, last, dob, phone, address):
+        self.first_name = first
+        self.last_name = last
+        self.date_ob_birth = dob
+        self.phone = phone
+        self.addresses = []
+
+        if isinstance(address, Address):
+            self.addresses.append(address)
+
+        elif isinstance(address, list):
+            for entry in address:
+                if not isinstance(entry, Address):
+                    raise Error("Invalid Address... ")
+
+            self.addresses = address
+
+        else:
+            raise Error("Invalid Address...")
+
+    def add_address(self, address):
+        if not isinstance(address, Address):
+            raise Error("invalid Address...")
+
+        self.addresses.append(address)
